9a09547ebf4730efd6194ef5c4f729033339b50e
DaJuan Harris <dajuancancode@gmail.com>
2020-07-18 23:57:26 -0400

    Finish final app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const cors =  require('cors')
-const fetch = require('node-fetch');
-const { response } = require("express");
+const axios = require('axios');
 
 const app = express()
 
@@ -13,13 +12,33 @@ app.get("/", (req, res) => {
   res.send("Hello World")
 })
 
-app.post("/api", (req, res) => {
+app.post("/api", async (req, res) => {
   const { country } = req.body
-  fetch(`https://restcountries.eu/rest/v2/name/${encodeURI(country)}`)
-    .then((response) => response.json())
-    .then((data) => res.json(data))
-    .catch((e) => console.log(e))
+
+  const splitCountry = country.split(" ")[0] + "-" + country.split(" ")[1]
+  
+
+  try {
+    const {data: countryData} = await axios(`https://restcountries.eu/rest/v2/name/${encodeURI(country)}`)
+    const {data: casesData} = await axios(`https://api.covid19api.com/total/country/${splitCountry}?from=2020-07-17T00:00:00Z&to=2020-07-19T00:00:00Z`)
+
+
+    
+    const countryToSend = {
+      name: countryData[0].name,
+      population: countryData[0].population,
+      flag: countryData[0].flag,
+      confirmed: casesData[0].Confirmed,
+      deaths: casesData[0].Deaths,
+      recovered: casesData[0].Recovered,
+    }
+
+    res.json([countryToSend])
+  } catch(e) { console.log(e)}
+
 })
+
+    
 
 const port = 8000
 app.listen(port, () => console.log(`[app]: Listening on port ${port}`))
